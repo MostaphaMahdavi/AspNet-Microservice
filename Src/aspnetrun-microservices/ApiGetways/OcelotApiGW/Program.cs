0ceb1d3666c18717c205631a7812a5eea774c86b
mostafa.mahdavi <mostapha@mostafamahdavis-MacBook-Pro.local>
2022-08-30 21:51:01 +0430

    add aggregate microservice

--- Src/aspnetrun-microservices/ApiGetways/OcelotApiGW/Program.cs
+++ Src/aspnetrun-microservices/ApiGetways/OcelotApiGW/Program.cs
@@ -20,7 +20,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
 builder.Configuration.AddJsonFile($"Ocelot.{builder.Environment.EnvironmentName}.json", true, true);
-//builder.Configuration.AddJsonFile($"Ocelot.Local.json", true, true);
+//builder.Configuration.AddJsonFile($"Ocelot.json", true, true);
 var app = builder.Build();
 
 await app.UseOcelot();
---
+++ Src/aspnetrun-microservices/ApiGetways/Shopping.Aggregator/Extensions/ServiceRegistry.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Polly;
+using Polly.Extensions.Http;
+using Shopping.Aggregator.Services;
+
+namespace Shopping.Aggregator.Extensions
+{
+    public static class ServiceRegistry
+    {
+
+        public static void  AddServiceRegistry(this IServiceCollection services,IConfiguration Configuration)
+        {
+
+           // services.AddTransient<LoggingDelegatingHandler>();
+
+            services.AddHttpClient<ICatalogService, CatalogService>(c =>
+                c.BaseAddress = new Uri(Configuration["ApiSettings:CatalogUrl"]))
+                //.AddHttpMessageHandler<LoggingDelegatingHandler>()
+                //.AddPolicyHandler(GetRetryPolicy())
+                //.AddPolicyHandler(GetCircuitBreakerPolicy())
+                ;
+
+            services.AddHttpClient<IBasketService, BasketService>(c =>
+                c.BaseAddress = new Uri(Configuration["ApiSettings:BasketUrl"]))
+               // .AddHttpMessageHandler<LoggingDelegatingHandler>()
+               // .AddPolicyHandler(GetRetryPolicy())
+               // .AddPolicyHandler(GetCircuitBreakerPolicy())
+                ;
+
+            services.AddHttpClient<IOrderService, OrderService>(c =>
+                c.BaseAddress = new Uri(Configuration["ApiSettings:OrderingUrl"]))
+               // .AddHttpMessageHandler<LoggingDelegatingHandler>()
+               // .AddPolicyHandler(GetRetryPolicy())
+               // .AddPolicyHandler(GetCircuitBreakerPolicy())
+                ;
+
+        }
+
+        private static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+    {
+        // In this case will wait for
+        //  2 ^ 1 = 2 seconds then
+        //  2 ^ 2 = 4 seconds then
+        //  2 ^ 3 = 8 seconds then
+        //  2 ^ 4 = 16 seconds then
+        //  2 ^ 5 = 32 seconds
+
+        return HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .WaitAndRetryAsync(
+                retryCount: 5,
+                sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                onRetry: (exception, retryCount, context) =>
+                {
+                  //  Log.Error($"Retry {retryCount} of {context.PolicyKey} at {context.OperationKey}, due to: {exception}.");
+                });
+    }
+
+
+        private static IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
+    {
+        return HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .CircuitBreakerAsync(
+                handledEventsAllowedBeforeBreaking: 5,
+                durationOfBreak: TimeSpan.FromSeconds(30)
+            );
+    }
+    }
+
+}
